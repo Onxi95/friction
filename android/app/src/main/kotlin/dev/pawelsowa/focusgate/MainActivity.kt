e07245bb88748bcbde7e03cc98b6bb0c1e8fa29f
Paweł Sowa <pawel@the-sowa.com>
2026-06-22 20:32:38 +0200

    chore: start from scratch

--- android/app/src/main/kotlin/dev/pawelsowa/focusgate/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package dev.pawelsowa.focusgate
-
-import com.facebook.react.ReactActivity
-
-class MainActivity : ReactActivity() {
-    override fun getMainComponentName(): String = "FocusGate"
-}
--- android/src/main/kotlin/dev/pawelsowa/focusgate/config/ConfigRepository.kt
+++
@@ -1,7 +0,0 @@
-package dev.pawelsowa.focusgate.config
-
-interface ConfigRepository {
-    fun read(): AppConfig
-
-    fun write(transform: (AppConfig) -> AppConfig): AppConfig
-}
